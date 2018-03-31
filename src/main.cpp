828d9cada33c2f1f3a4ea03ff25177395eb005a9
Peter Richards <prichardsf@gmail.com>
2018-03-31 18:27:26 -0400

    Removed "using namespace std", removed /r from .sh files,

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char *argv[]) {
 
-  cout << "Printing numbers 1 through 10 in parallel\n";
+  std::cout << "Printing numbers 1 through 10 in parallel\n";
   #pragma omp parallel for num_threads(4)
-  for (int i = 0; i < 10; ++i) {
-    cout << i << endl;
+  for (int i = 1; i < 11; i++) {
+    std::cout << i << "\n";
   }
 }
