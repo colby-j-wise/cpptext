916e902bcc1268deeb04115bf330f3c1f5a57bde
Peter Richards <prichardsf@gmail.com>
2018-04-24 17:31:42 -0400

    Initial Tests

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -10,6 +10,23 @@
 
 // unit test runner
 int main(int argc, char *argv[]) {
-  std::cout << "Running Unit Tests\n";
+  std::cout << "Running Unit Tests...\n";
+
+  UnitTest::testFilterWhitespaceLines("       ");
+  UnitTest::testFilterWhitespaceLines("");
   UnitTest::testLowercase("A CAPITALIZED SENTENCE");
+  UnitTest::testLowercase("");
+  UnitTest::testLowercase("99 RED BALOONS");
+  UnitTest::testLowercase("__C0LUMB1A_IN_NYC_><__=");
+  UnitTest::testRemovePunc("N.Y.C.");
+  UnitTest::testRemovePunc("<N><Y><C>");
+  UnitTest::testRemovePunc("\"What is your name?\" she said, hastily.");
+  UnitTest::testRemoveDigits("FINE");
+  UnitTest::testRemoveDigits("C0LUMB1A");
+  UnitTest::testRemoveDigits("__C0LUMB1A_IN_NYC___=");
+  UnitTest::testRemoveDigits("99 RED BALOONS");
+
+
+
+
 }
--- src/unit_test.h
+++ src/unit_test.h
@@ -8,18 +8,74 @@
 // Basic unit test class
 class UnitTest {
   public:
+
+    //Filter Whitespace Lines Test
+    static void testFilterWhitespaceLines(std::string input) {
+      static unsigned int test_number = 1;
+      Normalizer normalizer;
+      bool result = normalizer.whitespaceOnly(input);
+      if (result == false) {
+        std::cout << "[ ] || Filter Whitespace Lines Test " << test_number << " Failed! || Tested string: \"" << input << "\"\n";
+        test_number++;
+        return;
+      }
+      std::cout << "[X] || Filter Whitespace Lines Test " << test_number << " Passed\n";
+      test_number++;
+    }
+
+    //Lowercase Test
     static void testLowercase(std::string input) {
-      std::cout << "Testing Lowercasing\n";
+      static unsigned int test_number = 1;
       Normalizer normalizer;
       normalizer.lowercaseFunc(input);
       for (char c : input) {
         if (!islower(c) && !isspace(c)) {
-          std::cout << "Test Failed!\n";
+          std::cout << "[ ] || Lowercasing Test " << test_number << " Failed! || Tested string: \"" << input << "\"\n";
+          test_number++;
           return;
         }
       }
-      std::cout << "Test Passed\n";
+      std::cout << "[X] || Lowercasing Test " << test_number << " Passed\n";
+      test_number++;
     }
-    // Add more tests here, keep them static if possible
+
+    //Remove Punctuation Test
+    static void testRemovePunc(std::string input) {
+      static unsigned int test_number = 1;
+      Normalizer normalizer;
+      normalizer.removepuncFunc(input);
+      for (char c : input) {
+        if(ispunct(static_cast<unsigned char>(c))) {
+          std::cout << "[ ] || Remove Punctuation Test " << test_number << " Failed! || Tested string: \"" << input << "\"\n";
+          test_number++;
+          return;
+        }
+      }
+      std::cout << "[X] || Remove Punctuation Test " << test_number << " Passed\n";
+      test_number++;
+    }
+
+
+    //Remove Digits Test
+    static void testRemoveDigits(std::string input) {
+      static unsigned int test_number = 1;
+      Normalizer normalizer;
+      normalizer.removedigitFunc(input);
+      for (char c : input) {
+        if(isdigit(static_cast<unsigned char>(c))) {
+          std::cout << "[ ] || Remove Digits Test " << test_number << " Failed! || Tested string: \"" << input << "\"\n";
+          test_number++;
+          return;
+        }
+      }
+      std::cout << "[X] || Remove Digits Test " << test_number << " Passed\n";
+      test_number++;
+    }
+
+    //Set Stopwords Test
+
+    //Process Test 
+
+
   private:
 };
