30ab1a17cc5f8f326a3bd3e232fb2e94ccf2a4ea
colby-j-wise <colby.j.wise@gmail.com>
2018-04-24 23:38:05 -0400

    script mimicks library on simple test cases; need to test on data

---
+++ runtime_comparison.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# time on commandline or in script
+# write to new output 
+start=$(date +%s)
+for filename in ./runtime_testcases/*.txt; 
+do
+	[ -e "$filename" ] || continue
+	name=$(basename $filename)
+
+	# remove blank lines
+	< "$filename" awk 'NF > 0'  |
+
+	# Optional - remove regex i.e. <html> (missing multi whitespace)
+	awk -v RS='<[^>]+>' -v ORS= '1' |
+
+	# lowercase all files
+    awk '{print tolower($0)}' | 
+
+    # remove punctuation (replaces with "")
+    awk '{ gsub(/[[:punct:]]/, "", $0) } 1;' |
+
+    # remove digits
+    tr -d 0-9  > "./runtime_output/$name" 
+
+    # Optional: - remove stop words
+          
+done
+
+# Print run time
+end=$(date +%s) 
+runtime=$(python -c "print '%u:%02u' % ((${end} - ${start})/60, (${end} - ${start})%60)")
+echo "Runtime was $runtime"
+
